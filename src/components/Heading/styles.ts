8ba8644460c5744fbbc7d7f9ef556b21455c5373
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-20 14:28:44 -0300

    add Heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxxlarge};
+    position: relative;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 2.4rem;
+      height: 2.4rem;
+      position: absolute;
+      left: -0.5rem;
+      bottom: 1.5rem;
+      background: ${theme.colors.primary};
+      border-radius: ${theme.border.radius};
+      z-index: -1;
+    }
+  `}
+`
