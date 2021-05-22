80beb4b21963a8c9d3edf1fb5a3ac4c31e5d6bb1
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-22 19:20:26 -0300

    add Helmet to define the document title

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,16 +1,24 @@
 import * as S from './styles'
 
+import { Helmet } from 'react-helmet'
+
 import Base from '../../templates/Base'
 import Heading from '../../components/Heading'
 import Container from '../../components/Container'
 
 const Page404 = () => (
-  <Base>
-    <Container>
-      <Heading>Erro 404</Heading>
-      <S.Text>Página não encontrada.</S.Text>
-    </Container>
-  </Base>
+  <>
+    <Helmet>
+      <title>404 | Dogs</title>
+    </Helmet>
+
+    <Base>
+      <Container>
+        <Heading>Erro 404</Heading>
+        <S.Text>Página não encontrada.</S.Text>
+      </Container>
+    </Base>
+  </>
 )
 
 export default Page404
