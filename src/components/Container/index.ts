788b6c04e0cd18150320bac0b20c447db6fa96a9
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-22 14:27:40 -0300

    refactor Container component style

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components'
 const Container = styled.div`
   ${({ theme }) => css`
     max-width: 80rem;
+    width: 100%;
     padding: ${theme.spacings.xsmall};
     margin: 0 auto;
   `}
