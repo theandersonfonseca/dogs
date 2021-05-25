ace1884aaec15660da28223b29895c23c00d2b46
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-25 20:49:05 -0300

    remove unnecessary files

--- src/templates/Base/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  justify-content: space-between;
-  padding-top: 6.5rem;
-`
