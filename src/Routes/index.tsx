be1fa8a259ace8cd02afa9c0e123d98ef8ce9ac7
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-27 21:10:40 -0300

    add routes

---
+++ src/Routes/index.tsx
@@ -0,0 +1,31 @@
+import { Route, Switch } from 'react-router-dom'
+
+import Feed from '../pages/Feed'
+import SignIn from '../pages/SignIn'
+import ForgotPassword from '../pages/ForgotPassword'
+import ResetPassword from '../pages/ResetPassword'
+import SignUp from '../pages/SignUp'
+import Account from '../pages/Account'
+import Stats from '../pages/Stats'
+import PhotoPost from '../pages/PhotoPost'
+import Page404 from '../pages/404'
+
+import ProtectedRoute from './ProtectedRoute'
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Feed} exact />
+      <Route path="/login" component={SignIn} exact />
+      <Route path="/login/perdeu" component={ForgotPassword} exact />
+      <Route path="/login/resetar" component={ResetPassword} />
+      <Route path="/criar" component={SignUp} exact />
+      <Route path="/conta" component={Account} exact />
+      <ProtectedRoute path="/conta/estatisticas" component={Stats} exact />
+      <ProtectedRoute path="/conta/postar" component={PhotoPost} exact />
+      <Route path="*" component={Page404} />
+    </Switch>
+  )
+}
+
+export default Routes
