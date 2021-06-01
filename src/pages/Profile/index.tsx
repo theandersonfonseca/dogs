4e05e8ae2a40df5b82a6efff32c4c70ca746bd02
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-31 21:56:52 -0300

    add Profile component

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,30 @@
+import * as S from './styles'
+
+import { Helmet } from 'react-helmet'
+import { useParams } from 'react-router'
+
+import PageBase from '../../templates/PageBase'
+import Heading from '../../components/Heading'
+import Container from '../../components/Container'
+import Feed from '../../components/Feed'
+
+const Profile = () => {
+  const { user } = useParams<{ user?: string }>()
+
+  return (
+    <>
+      <Helmet>
+        <title>{`${user} | Dogs`}</title>
+      </Helmet>
+
+      <PageBase>
+        <Container>
+          <Heading>{user}</Heading>
+          <Feed user={user} />
+        </Container>
+      </PageBase>
+    </>
+  )
+}
+
+export default Profile
