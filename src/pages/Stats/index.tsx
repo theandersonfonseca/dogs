e503c4491fe89085ede1aad3e467551f2b766ffa
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-06-05 21:26:17 -0300

    add Stats page

---
+++ src/pages/Stats/index.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import * as S from './styles'
+
+import { Helmet } from 'react-helmet'
+
+import { STATS_GET } from '../../Api'
+
+import PageBase from '../../templates/PageBase'
+import Heading from '../../components/Heading'
+import Container from '../../components/Container'
+import AccountNav from '../../components/AccountNav'
+import Loading from '../../components/Loading'
+import StatsGraphs, { StatsType } from '../../components/StatsGraphs'
+
+const Stats = () => {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<StatsType[]>({} as StatsType[])
+
+  useEffect(() => {
+    setLoading(true)
+
+    const getStats = async () => {
+      const { url, options } = STATS_GET()
+
+      const response = await fetch(url, options)
+      const responseData = await response.json()
+
+      setData(responseData)
+      setLoading(false)
+    }
+
+    getStats()
+  }, [])
+
+  return (
+    <>
+      <Helmet>
+        <title>Estatísticas | Dogs</title>
+      </Helmet>
+
+      <PageBase>
+        <Container>
+          <S.Wrapper>
+            <Heading>Estatísticas</Heading>
+
+            <AccountNav id="stats" />
+          </S.Wrapper>
+
+          {loading ? (
+            <Loading />
+          ) : (
+            <S.GraphWrapper>
+              <StatsGraphs data={data} />
+            </S.GraphWrapper>
+          )}
+        </Container>
+      </PageBase>
+    </>
+  )
+}
+
+export default Stats
